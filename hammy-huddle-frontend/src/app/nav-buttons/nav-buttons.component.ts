9a956c28d00e4484a8a62fe9a4f30d9c279ff7c7
Savina Chan <savinachan1582@gmail.com>
2024-07-12 16:43:23 -0400

    set up home page

---
+++ hammy-huddle-frontend/src/app/nav-buttons/nav-buttons.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-buttons',
+  templateUrl: './nav-buttons.component.html',
+  styleUrl: './nav-buttons.component.scss'
+})
+export class NavButtonsComponent {
+
+}
