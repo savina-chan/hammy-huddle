ec0fa5d8cc0584459314b53105310cfea97657dc
Savina Chan <savinachan1582@gmail.com>
2024-07-12 15:35:34 -0400

    set up pages and routing

--- hammy-huddle-frontend/src/app/app-routing.module.ts
+++ hammy-huddle-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { GuidePageComponent } from './guide-page/guide-page.component';
+import { ForumPageComponent } from './forum-page/forum-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomePageComponent},
+  {path:"login", component:LoginPageComponent},
+  {path:"register", component:RegisterPageComponent},
+  {path:"profile", component:ProfilePageComponent},
+  {path:"guides", component:GuidePageComponent},
+  {path:"forum", component:ForumPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- hammy-huddle-frontend/src/app/app.module.ts
+++ hammy-huddle-frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { GuidePageComponent } from './guide-page/guide-page.component';
+import { ForumPageComponent } from './forum-page/forum-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    GuidePageComponent,
+    ForumPageComponent,
+    ProfilePageComponent
   ],
   imports: [
     BrowserModule,
