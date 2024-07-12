a56237918c0daeffa1ffd90cfc64ec19d00813de
Savina Chan <savinachan1582@gmail.com>
2024-07-12 17:04:42 -0400

    set up login page form

--- hammy-huddle-frontend/src/app/app.module.ts
+++ hammy-huddle-frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
@@ -28,7 +29,9 @@ import { NavButtonsComponent } from './nav-buttons/nav-buttons.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- hammy-huddle-frontend/src/app/login-page/login-page.component.ts
+++ hammy-huddle-frontend/src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  
+  formLogin = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)])
+  });
 
+  get email() {
+    return this.formLogin.get('email');
+  }
+
+  get password() {
+    return this.formLogin.get('password');
+  }
+
+  getEmail() {
+    return this.formLogin.get('email')?.value || '';
+  }
+
+  getPassword() {
+    return this.formLogin.get('password')?.value || '';
+  }
 }
