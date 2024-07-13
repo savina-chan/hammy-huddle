3e81810389b417b515d7c1746695eba4e7850466
Savina Chan <savinachan1582@gmail.com>
2024-07-12 23:55:13 -0400

    edited favicon and title, set up register page, and edited styling for lr buttons and login page

--- hammy-huddle-frontend/src/app/register-page/register-page.component.ts
+++ hammy-huddle-frontend/src/app/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
@@ -6,5 +7,54 @@ import { Component } from '@angular/core';
   styleUrl: './register-page.component.scss'
 })
 export class RegisterPageComponent {
+  formRegister!: FormGroup;
+  passwordFieldType: string = 'password';
 
+  ngOnInit() {
+    this.formRegister = new FormGroup({
+        username: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.minLength(8), this.complexityValidator()])
+    });
+  }
+
+  togglePasswordVisibility(): void {
+    this.passwordFieldType = this.passwordFieldType === 'password' ? 'text' : 'password';
+  }
+
+  get username() {
+    return this.formRegister.get("username");
+  }
+
+  get email() {
+    return this.formRegister.get("email");
+  }
+
+  get password() {
+    return this.formRegister.get("password");
+  }
+
+  getUsername() {
+    return this.formRegister.get("username")?.value || '';
+  }
+
+  getEmail() {
+    return this.formRegister.get("email")?.value || '';
+  }
+
+  getPassword() {
+    return this.formRegister.get("password")?.value|| '';
+  }
+
+  complexityValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+      const hasUpperCase = /[A-Z]/.test(value);
+      const hasLowerCase = /[a-z]/.test(value);
+      const hasNumber = /[0-9]/.test(value);
+      const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
+      const passwordValid = hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar;
+      return !passwordValid ? { complexity: true } : null;
+    };
+  }
 }
