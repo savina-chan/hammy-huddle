a97435dd1f0e4cc736119b7c00cfe51053342438
elphan <starsoccerethan@gmail.com>
2024-07-13 16:51:00 -0400

    created login and register api endpoints

--- hammy-huddle-frontend/src/app/login-page/login-page.component.ts
+++ hammy-huddle-frontend/src/app/login-page/login-page.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
+import { supervisor } from '../supervisor.service';
+import { Login } from '../login';
 
 @Component({
   selector: 'app-login-page',
@@ -12,6 +16,10 @@ export class LoginPageComponent {
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(8)])
   });
+
+  constructor( private apiService: ApiService, private router: Router) {}
+
+  failedLogin = false;
 
   get email() {
     return this.formLogin.get('email');
@@ -28,4 +36,24 @@ export class LoginPageComponent {
   getPassword() {
     return this.formLogin.get('password')?.value || '';
   }
-}
+
+  postToDB(event: Event){
+
+    event.preventDefault();
+
+    const payload = {
+      email: this.getEmail(),
+      password: this.getPassword()
+    };
+      
+    this.apiService.sendLogin(payload)
+      .subscribe((response: Login) => {       
+        // Set token expiry to 1 Day => Require Re-login
+        supervisor.setItem('username', response.username, 1440);
+        supervisor.setItem('token', response.accessToken, 1440);
+        this.router.navigate(['/']);
+      }, error => {
+        this.failedLogin = true;
+    });
+  }
+}   
